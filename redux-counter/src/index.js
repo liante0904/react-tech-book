082c833ddc01c3f8b99cd65bcaf9d081bf10afff
seunghoon shin <liante0904@gmail.com>
2018-09-26 21:50:55 +0900

    read chapter13

---
+++ redux-counter/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+
+
+// 리덕스 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+// 스토어 생성
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+ document.getElementById('root')
+ );
+registerServiceWorker();
