3552e673a3eef6517d3bdb8e50222a8d2047c9ff
shinseunghoon <liante0904@gmail.com>
2018-10-03 23:12:51 +0900

    review chapter14

--- todo-list-redux/src/modules/input.js
+++ todo-list-redux/src/modules/input.js
@@ -1,7 +1,6 @@
 import { Map } from 'immutable';
 import { handleActions, createAction } from 'redux-actions';
 
-
 const SET_INPUT = 'input/SET_INPUT';
 
 export const setInput = createAction(SET_INPUT);
