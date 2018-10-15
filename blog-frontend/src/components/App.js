6650ccb619b3314b9ec270c772e41c2aac25b293
seunghoon shin <liante0904@gmail.com>
2018-10-15 23:50:20 +0900

    chapter12 blog add router

--- blog-frontend/src/components/App.js
+++ blog-frontend/src/components/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { ListPage, PostPage, EditorPage, NotFoundPage } from 'pages';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                BlogApp        
-            </div>
-        );
-    }
+const App = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={ListPage} />
+            <Route path="/page/:page" component={ListPage}/>
+            <Route path="/tag/:tag/:page?" component={ListPage}/>
+            <Route path="/post:/id" component={PostPage}/>
+            <Route path="/editor" component={EditorPage}/>
+            <Route component={NotFoundPage}/>
+        </Switch>
+    );
 }
-
 export default App;
